b8081693aed68c55982a115a00980f5dee6eb88d
Anas Daas <anasdaas210@gmail.com>
2024-12-30 14:22:52 +0300

    edit settings

--- klinikler/settings.py
+++ klinikler/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
+    "http://localhost:3000",
     "http://127.0.0.1:8002",
     "http://localhost:8002",
     "https://app.tzuchitech.com",
